bcc7003024e6f33df4b1d6123792b8686eddd749
kanwei@gmail.com <kanwei@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-14 21:04:03 +0000

    Tagging Version 2.6.0
    git-svn-id: https://plugins.svn.wordpress.org/showcase-idx/tags/2.6.0@1198986 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets.php
+++ widgets.php
@@ -4,12 +4,17 @@
 // curried shortcode generators
 function showcaseidx_widget_login() { return showcaseidx_generate_app(null, "/login"); }
 function showcaseidx_widget_register() { return showcaseidx_generate_app(null, "/register"); }
-function showcaseidx_widget_230() { return showcaseidx_generate_widget('widgets230'); }
-function showcaseidx_widget_465() { return showcaseidx_generate_widget('widgets465'); }
-function showcaseidx_widget_700() { return showcaseidx_generate_widget('widgets700'); }
-function showcaseidx_widget_930() { return showcaseidx_generate_widget('widgets930'); }
-function showcaseidx_widget_omnibox() { return showcaseidx_generate_widget('widgetsomnibox'); }
-function showcaseidx_widget_updated() { return showcaseidx_generate_widget('widgetsupdated'); }
+function showcaseidx_widget_230() { return showcaseidx_generate_widget('widgets/230'); }
+function showcaseidx_widget_465() { return showcaseidx_generate_widget('widgets/465'); }
+function showcaseidx_widget_700() { return showcaseidx_generate_widget('widgets/700'); }
+function showcaseidx_widget_930() { return showcaseidx_generate_widget('widgets/930'); }
+function showcaseidx_widget_contact() { return showcaseidx_generate_widget('widgets/contact'); }
+function showcaseidx_widget_omnibox() { return showcaseidx_generate_widget('widgets/omnibox'); }
+function showcaseidx_widget_updated() { return showcaseidx_generate_widget('widgets/updated'); }
+function showcaseidx_widget_featured() { return showcaseidx_generate_widget('widgets/listing/featured'); }
+function showcaseidx_widget_agent() { return showcaseidx_generate_widget('widgets/listing/agent'); }
+function showcaseidx_widget_office() { return showcaseidx_generate_widget('widgets/listing/office'); }
+function showcaseidx_widget_hotsheet($scParams) { return showcaseidx_generate_widget("widgets/listing/hotsheet?name=$scParams"); }
 function showcaseidx_generate_widget($type)
 {
     $config = showcaseidx_generate_config();
@@ -26,9 +31,9 @@ function showcaseidx_generate_widget($type)
 EOT;
 }
 
-function showcaseidx_show_app() {
+function showcaseidx_show_app($scParams) {
     $seoPlaceholder = '<noscript><a href="' . showcaseidx_base_url() . '/all">View all listings</a></noscript>';
-    return showcaseidx_generate_app($seoPlaceholder);
+    return showcaseidx_generate_app($seoPlaceholder, NULL, NULL, $scParams = $scParams);
 }
 
 function showcaseidx_show_hotsheet($scParams) {
@@ -51,12 +56,12 @@ function showcaseidx_show_hotsheet($scParams) {
 }
 
 /*************** HELPER FUNCTIONS FOR SHORTCODE GENERATORS **********************/
-function showcaseidx_generate_app($seoPlaceholder = NULL, $defaultAppUrl = NULL, $customSearchConfig = NULL) {
+function showcaseidx_generate_app($seoPlaceholder = NULL, $defaultAppUrl = NULL, $customSearchConfig = NULL, $scParams = NULL) {
     if ($customSearchConfig === NULL)
     {
         $customSearchConfig = showcaseidx_get_custom_widget_config();
     }
-    $config = showcaseidx_generate_config($customSearchConfig);
+    $config = showcaseidx_generate_config($customSearchConfig, $scParams);
     $cdn_host = get_option('showcaseidx_cdn_host');
     $defaultAppUrl = $defaultAppUrl ? showcaseidx_generate_default_app_url($defaultAppUrl) : NULL;
     $widget = apply_filters('showcase_widget_content', showcaseidx_cachable_fetch("$cdn_host/wordpress_noscript"));
@@ -70,7 +75,7 @@ function showcaseidx_generate_app($seoPlaceholder = NULL, $defaultAppUrl = NULL,
 EOT;
 }
 
-function showcaseidx_generate_config($customSearchConfig = null) {
+function showcaseidx_generate_config($customSearchConfig = null, $scParams = NULL) {
     $api_key = get_option('showcaseidx_api_key');
     $api_root = get_option('showcaseidx_api_v2_host');
     $cdn_root = get_option('showcaseidx_cdn_host');
@@ -78,6 +83,12 @@ function showcaseidx_generate_config($customSearchConfig = null) {
     if ($customSearchConfig === NULL)
     {
         $customSearchConfig = 'null';
+    }
+    if ($scParams === NULL)
+    {
+        $scParams = 'null';
+    } else {
+        $scParams = json_encode($scParams);
     }
 
     return <<<EOT
@@ -87,11 +98,14 @@ if (!SHOWCASE_CONF) {
     var SHOWCASE_CONF = {
         WEBSITE_ROOT: "{$api_root}",
         CDN_ROOT: "{$cdn_root}",
-        WEBSITE_ID: "{$api_key}"
+        WEBSITE_ID: "{$api_key}",
     };
 }
 if ($customSearchConfig) {
     SHOWCASE_CONF.SEARCH_CONF = $customSearchConfig;
+}
+if ($scParams) {
+    SHOWCASE_CONF.scParams = $scParams;
 }
 </script>
 EOT;
